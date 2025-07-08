79e26112febd9ebdec994199662db3a5840aa542
Aarav Arora <aarav.arora2500@gmail.com>
2025-07-08 15:36:00 +0530

    deployment changes for backend

--- Backend/db/mikro-orm.config.js
+++ Backend/db/mikro-orm.config.js
@@ -6,11 +6,6 @@ dotenv.config();
 
 export default defineConfig({
   entities: [schema],
-  dbName: process.env.DB_NAME,
-  driverOptions: {},
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  debug: true,
+  clientUrl: process.env.DATABASE_URL,
+  debug: process.env.NODE_ENV !== 'production',
 });
