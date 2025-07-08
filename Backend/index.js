f1ef87ab1ef0a5fc9847b1c76198c829e776d510
Aarav Arora <aarav.arora2500@gmail.com>
2025-07-08 22:41:21 +0530

    project deployed

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,7 @@ const start = async () => {
   orm = await MikroORM.init(mikroConfig);
   await orm.getSchemaGenerator().updateSchema();
 
+    app.get("/ping", (req, res) => res.send("pong"));
   //Use full CRUD /history routes
   app.use('/history', requestLogRoutes(orm));
 
